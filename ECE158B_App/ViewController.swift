dca58d51baec5a8622db7545aaef17d2d4779f0e
Kenneth Harvey <kenny.harvey@icloud.com>
2016-03-30 23:12:04 -0700

    Initial Commit

---
+++ ECE158B_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECE158B_App
+//
+//  Created by Kenneth Harvey on 3/30/16.
+//  Copyright © 2016 Richard Harvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
